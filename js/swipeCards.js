36ced4a409d6a6cb2a9871ad28462ee402d9152b
Robocotik <alexander.starkin@gmail.com>
2024-02-09 12:33:30 +0300

    [FIXED] anim time and added arrow align

--- js/swipeCards.js
+++ js/swipeCards.js
@@ -9,23 +9,54 @@ prevBtn.addEventListener('click', swipeToPrev);
 
 var curPage = 1;
 var maxCountPage = 5;
+var animDuration = 600 //0.5s
+
+function downScaleOpacityAnim(){
+    let curCard = document.querySelector(".active")
+    curCard.style.opacity = "0"
+    curCard.style.transition = animDuration/1000 + 's'
+    
+}
+
+function upScaleOpacityAnim(){
+    let curCard = document.querySelector(".active")
+    curCard.style.opacity = "1"
+    curCard.style.transition = animDuration/1000 + 's'
+    
+}
+
+function doSwipe_next(){
+    let curCard = document.querySelector("#card" + curPage)
+    curCard.classList.remove("active")
+    curPage++;
+    let swipedCard = document.querySelector("#card" + curPage)
+    swipedCard.classList.add("active")
+    swipedCard.style.opacity = "0"
+}
+
+function doSwipe_prev(){
+    let curCard = document.querySelector("#card" + curPage)
+    curCard.classList.remove("active")
+    curPage--;
+    let swipedCard = document.querySelector("#card" + curPage)
+    swipedCard.classList.add("active")
+    swipedCard.style.opacity = "0"
+}
+
 function swipeToNext(){
     if (curPage < maxCountPage){
-        let curCard = document.querySelector("#card" + curPage)
-        curCard.classList.remove("active")
-        curPage++;
-        let swipedCard = document.querySelector("#card" + curPage)
-        swipedCard.classList.add("active")
+        downScaleOpacityAnim();
+        setTimeout(doSwipe_next,animDuration);
+        setTimeout(upScaleOpacityAnim,animDuration+ 50);
+
     }
 }
 
 function swipeToPrev(){
     if (curPage > 1){
-        let curCard = document.querySelector("#card" + curPage)
-        curCard.classList.remove("active")
-        curPage--;
-        let swipedCard = document.querySelector("#card" + curPage)
-        swipedCard.classList.add("active")
+        downScaleOpacityAnim();
+        setTimeout(doSwipe_prev,animDuration);
+        setTimeout(upScaleOpacityAnim,animDuration + 50);
     }
 }
 
